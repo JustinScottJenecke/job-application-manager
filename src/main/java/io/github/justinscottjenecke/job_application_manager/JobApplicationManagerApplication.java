0d7487bdf06f2091c5e56fb5cf9f29fb795dfa8b
justinscottjenecke <justinjenecke@gmail.com>
2024-09-18 13:29:03 +0200

    init-commit: creates new spring web project /w lombok

---
+++ src/main/java/io/github/justinscottjenecke/job_application_manager/JobApplicationManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.justinscottjenecke.job_application_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/justinscottjenecke/job_application_manager/JobApplicationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.justinscottjenecke.job_application_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
