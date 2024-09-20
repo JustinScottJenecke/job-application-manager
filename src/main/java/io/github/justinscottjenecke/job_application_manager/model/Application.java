976aaffab235568a0fdaf42521e6486448adea47
justinscottjenecke <justinjenecke@gmail.com>
2024-09-20 13:27:07 +0200

    feat: adds relational annotations to job and application

--- src/main/java/io/github/justinscottjenecke/job_application_manager/model/Application.java
+++ src/main/java/io/github/justinscottjenecke/job_application_manager/model/Application.java
@@ -1,16 +1,24 @@
 package io.github.justinscottjenecke.job_application_manager.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @OneToOne
+    private Job job;
+
     private Double costToCompany; // could be moved to profile
     private ApplicationStatus applicationStatus; // enum(not applied, applied, no response, interview, assessment, rejected, accepted)
     private String applicationStatusNotes; // metadata on interview number, accepted an offer, etc.
--- src/main/java/io/github/justinscottjenecke/job_application_manager/model/Job.java
+++ src/main/java/io/github/justinscottjenecke/job_application_manager/model/Job.java
@@ -1,14 +1,23 @@
 package io.github.justinscottjenecke.job_application_manager.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @OneToOne
+    @JoinColumn
+    private Application application;
+
     private String position;
     private String company;
     private String location;
