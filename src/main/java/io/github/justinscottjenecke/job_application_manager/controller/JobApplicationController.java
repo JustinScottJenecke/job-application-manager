5d2c6c3d8caa74005b0c669141f3b01426271d3a
justinscottjenecke <justinjenecke@gmail.com>
2024-09-21 22:43:32 +0200

    feat: removes data.sql & creates seed endpoint for testing mock data against db

--- src/main/java/io/github/justinscottjenecke/job_application_manager/controller/JobApplicationController.java
+++ src/main/java/io/github/justinscottjenecke/job_application_manager/controller/JobApplicationController.java
@@ -1,15 +1,44 @@
 package io.github.justinscottjenecke.job_application_manager.controller;
 
+import io.github.justinscottjenecke.job_application_manager.model.Job;
+import io.github.justinscottjenecke.job_application_manager.model.WorkModel;
+import io.github.justinscottjenecke.job_application_manager.repository.IJobRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class JobApplicationController {
+
+    private final IJobRepository jobRepository;
+
+    public JobApplicationController(IJobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
 
     @GetMapping("/")
     public String test() {
         return "test";
     }
 
+    @GetMapping("/job/seed")
+    public boolean seedJobs() {
 
+        Job savedJob = jobRepository.save(
+                new Job(
+                    1,
+                    null,
+                    "Junior Software Developer https://betsoftware.simplify.hr/Vacancy/95516",
+                    "BET Software",
+                    "Johannesburg, Gauteng",
+                    WorkModel.HYBRID,
+                    null,
+                    "C# * Design Patterns * .NET * SQL",
+                    "https://betsoftware.simplify.hr/Vacancy/95516")
+        );
+
+        return jobRepository.existsById(savedJob.getId());
+    }
 }
