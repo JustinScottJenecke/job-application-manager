3e8e96f3cdfe137b6c0ca6fe5be8756ba0ae7141
justinscottjenecke <justinjenecke@gmail.com>
2024-09-22 21:06:55 +0200

    feat: adds functionality for creating new Application

--- src/main/java/io/github/justinscottjenecke/job_application_manager/controller/ApplicationController.java
+++ src/main/java/io/github/justinscottjenecke/job_application_manager/controller/ApplicationController.java
@@ -1,10 +1,13 @@
 package io.github.justinscottjenecke.job_application_manager.controller;
 
+import io.github.justinscottjenecke.job_application_manager.dto.application.CreateApplicationDto;
 import io.github.justinscottjenecke.job_application_manager.model.Application;
+import io.github.justinscottjenecke.job_application_manager.model.enumerations.ApplicationStatus;
 import io.github.justinscottjenecke.job_application_manager.repository.IApplicationRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.justinscottjenecke.job_application_manager.repository.IJobRepository;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,9 +16,11 @@ import java.util.List;
 public class ApplicationController {
 
     private final IApplicationRepository applicationRepository;
+    private  final IJobRepository jobRepository;
 
-    public ApplicationController(IApplicationRepository applicationRepository) {
+    public ApplicationController(IApplicationRepository applicationRepository, IJobRepository jobRepository) {
         this.applicationRepository = applicationRepository;
+        this.jobRepository = jobRepository;
     }
 
     @GetMapping("/test")
@@ -26,6 +31,21 @@ public class ApplicationController {
     @GetMapping
     public List<Application> readAll() {
         return applicationRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<String> create(@RequestBody CreateApplicationDto applicationDto) {
+        Application application = new Application();
+
+        application.setJob(jobRepository.getReferenceById( applicationDto.jobId() ));
+        application.setApplicationStatus( ApplicationStatus.valueOf(applicationDto.applicationStatus()) );
+        application.setApplicationStatusNotes(applicationDto.applicationStatusNotes());
+        application.setDateApplied(applicationDto.dateApplied());
+        application.setCostToCompany(applicationDto.costToCompany());
+
+        applicationRepository.save(application);
+
+        return new ResponseEntity<>("New job applied for.", HttpStatusCode.valueOf(201));
     }
 
 }
