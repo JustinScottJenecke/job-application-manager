2d972cbf3b1385787f6642630c0dfeb8f9c2a258
justinscottjenecke <justinjenecke@gmail.com>
2024-10-04 14:52:28 +0200

    feature: creates implementation for mapping jobdetailsdto to job

--- src/main/java/io/github/justinscottjenecke/job_application_manager/service/JobService.java
+++ src/main/java/io/github/justinscottjenecke/job_application_manager/service/JobService.java
@@ -2,20 +2,27 @@ package io.github.justinscottjenecke.job_application_manager.service;
 
 import io.github.justinscottjenecke.job_application_manager.dto.job.CreateJobDto;
 import io.github.justinscottjenecke.job_application_manager.dto.job.JobDetailsDto;
+import io.github.justinscottjenecke.job_application_manager.model.Application;
 import io.github.justinscottjenecke.job_application_manager.model.Job;
+import io.github.justinscottjenecke.job_application_manager.model.enumerations.WorkModel;
+import io.github.justinscottjenecke.job_application_manager.repository.IApplicationRepository;
 import io.github.justinscottjenecke.job_application_manager.repository.IJobRepository;
+import org.hibernate.annotations.NotFound;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 @Service
 public class JobService {
 
     private final IJobRepository jobRepository;
+    private final IApplicationRepository applicationRepository;
 
-    public JobService(IJobRepository jobRepository) {
+    public JobService(IJobRepository jobRepository, IApplicationRepository applicationRepository) {
         this.jobRepository = jobRepository;
+        this.applicationRepository = applicationRepository;
     }
 
     public boolean create(CreateJobDto dto) {
@@ -31,6 +38,11 @@ public class JobService {
     }
 
     public boolean updateById(JobDetailsDto dto, Integer id) {
+
+        // check if id of job exists
+        // check if has application
+        // check if application is valid
+
         return false;
     }
 
@@ -40,8 +52,30 @@ public class JobService {
 
     // DTO Mapper functions
 
-    public void mapJobDetailsToModel(JobDetailsDto dto) {
+    /**
+     * Maps an incoming JobDetailsDto to a Job model. Typically, runs when updating an existing job
+     * @param dto JobDetailsDto
+     * @return Job
+     */
+    public Job mapJobDetailsToModel(JobDetailsDto dto) {
 
+        var job = new Job();
+
+        job.setId(dto.id());
+        job.setPosition(dto.position());
+        job.setCompany(dto.company());
+        job.setLocation(dto.location());
+        job.setWorkModel( WorkModel.valueOf(dto.workModel()) );
+        job.setPostedSalary(dto.postedSalary());
+        job.setCoreSkill(dto.coreSkill());
+        job.setRequiredSkillsAndTools(dto.requiredSkillsAndTools());
+        job.setJobPostingUrl(dto.jobPostingUrl());
+
+        // only set application if job has been applied for
+        if (dto.onGoingApplication())
+            job.setApplication( applicationRepository.findById(dto.id()).orElseThrow() );
+
+        return job;
     }
 
     public void mapCreateJobToModel(CreateJobDto dto) {
