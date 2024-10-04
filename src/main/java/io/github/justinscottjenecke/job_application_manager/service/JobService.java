d52fe98ef8e2b6df15be91a3cf53badd50de5920
justinscottjenecke <justinjenecke@gmail.com>
2024-10-04 20:58:55 +0200

    feature: adds java-doc to JobService CRUD methods

--- src/main/java/io/github/justinscottjenecke/job_application_manager/service/JobService.java
+++ src/main/java/io/github/justinscottjenecke/job_application_manager/service/JobService.java
@@ -23,6 +23,11 @@ public class JobService {
         this.applicationRepository = applicationRepository;
     }
 
+    /**
+     * Creates and persists a new Job entity into the database
+     * @param dto CreateJobDto
+     * @return Job
+     */
     public boolean create(CreateJobDto dto) {
 
         var job = jobRepository.save( mapCreateJobToModel(dto) );
@@ -30,16 +35,35 @@ public class JobService {
         return jobRepository.existsById(job.getId());
     }
 
+    /**
+     * Loads a single Job entity from the database using an id as a search parameter.
+     * Throws an EntityNotFound Exception if the id does not match the id of any existing Job entity
+     * @param id Integer
+     * @return Job
+     */
     public Job readById(Integer id) {
 
         return jobRepository.findById(id)
                 .orElseThrow( () -> new EntityNotFoundException("No Job entity found with given id: " + id));
     }
 
+    /**
+     * Returns a List of all Job entities stored within the Database
+     * @return List<Job>
+     */
     public List<Job> readAll() {
         return jobRepository.findAll();
     }
 
+    /**
+     * Updates any existing Job entity with properties of an incoming Dto's fields.
+     * If the given id does not match the id of any Job entities in the database, the method
+     * returns false, otherwise the method returns true upon a successful update.
+     *
+     * @param dto JobDetailsDto
+     * @param id Integer
+     * @return Boolean
+     */
     public boolean updateById(JobDetailsDto dto, Integer id) {
 
         // check if id of job exists - check if has application - check if application is valid
@@ -50,6 +74,12 @@ public class JobService {
         return false;
     }
 
+    /**
+     * Deletes a Job entity by id. Returns false in the event that a Job is unsuccessfully deleted
+     * or if the id given goes not match the id of any entity in the system.
+     * @param id Integer
+     * @return Boolean
+     */
     public boolean deleteById(Integer id) {
 
         if (!jobRepository.existsById(id))
