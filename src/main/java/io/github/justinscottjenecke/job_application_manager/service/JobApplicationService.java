4c1b4c565b709fa836a72c6c1df3b48f0b6a3bbe
justinscottjenecke <justinjenecke@gmail.com>
2024-10-05 21:51:40 +0200

    feat: adds dependancies for JobApplicationService and contructor dependancy injection

--- src/main/java/io/github/justinscottjenecke/job_application_manager/service/JobApplicationService.java
+++ src/main/java/io/github/justinscottjenecke/job_application_manager/service/JobApplicationService.java
@@ -1,8 +1,23 @@
 package io.github.justinscottjenecke.job_application_manager.service;
 
+import io.github.justinscottjenecke.job_application_manager.dto.jobApplication.JobApplicationWrapper;
+import io.github.justinscottjenecke.job_application_manager.repository.IApplicationRepository;
+import io.github.justinscottjenecke.job_application_manager.repository.IJobRepository;
+import io.github.justinscottjenecke.job_application_manager.service.mappers.JobApplicationMapper;
+
 public class JobApplicationService {
 
-    private static class Mapper {
+    private final IJobRepository jobRepository;
+    private final IApplicationRepository applicationRepository;
+    private final JobApplicationMapper mapper;
 
+    public JobApplicationService(
+            IJobRepository jobRepository,
+            IApplicationRepository applicationRepository,
+            JobApplicationMapper mapper
+    ) {
+        this.jobRepository = jobRepository;
+        this.applicationRepository = applicationRepository;
+        this.mapper = mapper;
     }
 }
