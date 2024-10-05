3189bda7437c1075de7ebf20c4c079ff59a9acc5
justinscottjenecke <justinjenecke@gmail.com>
2024-10-05 22:29:29 +0200

    adds implementation for create new Jobs and Applications from a single JobApplicationDto

--- src/main/java/io/github/justinscottjenecke/job_application_manager/service/JobApplicationService.java
+++ src/main/java/io/github/justinscottjenecke/job_application_manager/service/JobApplicationService.java
@@ -1,9 +1,12 @@
 package io.github.justinscottjenecke.job_application_manager.service;
 
-import io.github.justinscottjenecke.job_application_manager.dto.jobApplication.JobApplicationWrapper;
+import io.github.justinscottjenecke.job_application_manager.dto.jobApplication.CreateJobApplicationDto;
+import io.github.justinscottjenecke.job_application_manager.dto.jobApplication.JobApplicationDto;
 import io.github.justinscottjenecke.job_application_manager.repository.IApplicationRepository;
 import io.github.justinscottjenecke.job_application_manager.repository.IJobRepository;
 import io.github.justinscottjenecke.job_application_manager.service.mappers.JobApplicationMapper;
+
+import java.util.List;
 
 public class JobApplicationService {
 
@@ -20,4 +23,15 @@ public class JobApplicationService {
         this.applicationRepository = applicationRepository;
         this.mapper = mapper;
     }
+
+    public boolean create(CreateJobApplicationDto dto) {
+
+        var jobApplicationDto = mapper.mapCreateJobApplicationToModels(dto);
+
+        var job = jobRepository.save(jobApplicationDto.job());
+        var application = applicationRepository.save(jobApplicationDto.application());
+
+        return jobRepository.existsById(job.getId()) || applicationRepository.existsById(application.getId());
+    }
+
 }
