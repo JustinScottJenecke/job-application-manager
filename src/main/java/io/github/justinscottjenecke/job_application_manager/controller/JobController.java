ac56312778d6f3f208d331686e7271fd60c5a815
justinscottjenecke <justinjenecke@gmail.com>
2024-10-11 10:10:49 +0200

    feature: adds implmentations to JobController endpoint handlers + removes repository references and unused imports

--- src/main/java/io/github/justinscottjenecke/job_application_manager/controller/JobController.java
+++ src/main/java/io/github/justinscottjenecke/job_application_manager/controller/JobController.java
@@ -3,8 +3,8 @@ package io.github.justinscottjenecke.job_application_manager.controller;
 import io.github.justinscottjenecke.job_application_manager.dto.job.CreateJobDto;
 import io.github.justinscottjenecke.job_application_manager.dto.job.JobDetailsDto;
 import io.github.justinscottjenecke.job_application_manager.model.Job;
-import io.github.justinscottjenecke.job_application_manager.model.enumerations.WorkModel;
 import io.github.justinscottjenecke.job_application_manager.repository.IJobRepository;
+import io.github.justinscottjenecke.job_application_manager.service.JobService;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,34 +15,48 @@ import java.util.List;
 @RequestMapping("/api/v1/job")
 public class JobController {
 
-    private final IJobRepository jobRepository;
+    private final JobService jobService;
 
-    public JobController(IJobRepository jobRepository) {
-        this.jobRepository = jobRepository;
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
     }
 
     @PostMapping()
     public ResponseEntity<String> create(@RequestBody CreateJobDto jobDto) {
-        return new ResponseEntity<>("create", HttpStatusCode.valueOf(201));
+
+        jobService.create(jobDto);
+
+        return new ResponseEntity<>(
+                "Created new Job: " + jobDto.position() + " at " + jobDto.company(),
+                HttpStatusCode.valueOf(201)
+        );
     }
 
     @GetMapping()
     public List<Job> readAll() {
-        return null;
+        return jobService.readAll();
     }
 
     @GetMapping("/{id}")
     public Job read(@PathVariable Integer id) {
-        return null;
+        return jobService.readById(id);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<String> update(@RequestBody JobDetailsDto dto, @PathVariable Integer id) {
-        return new ResponseEntity<>("update", HttpStatusCode.valueOf(200));
+
+        return jobService.updateById(dto, id) ?
+            new ResponseEntity<>("Job successfully updated.", HttpStatusCode.valueOf(200))
+            :
+            new ResponseEntity<>("No Job with " + id + " exists in system.", HttpStatusCode.valueOf(404));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(@PathVariable Integer id) {
-        return new ResponseEntity<>("delete", HttpStatusCode.valueOf(200));
+
+        return jobService.deleteById(id) ?
+            new ResponseEntity<>("Job successfully deleted.", HttpStatusCode.valueOf(200))
+            :
+            new ResponseEntity<>("No Job with " + id + " exists in system or error deleting Job.", HttpStatusCode.valueOf(40));
     }
 }
